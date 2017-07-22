0ffdfd7496551efe7c6f8babb3511e7226cf8772
Rainer Spittel <rainer.spittel@tepapa.govt.nz>
2017-07-23 11:41:10 +1200

    MINOR fixing docker install problem, caused by the new version on the Jenkins LTD image.

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -2,10 +2,13 @@
 
 IMAGE=rasp/jenkins
 
-docker run  -it \
+docker run  -it --rm \
+            -v $HOME/.ssh/id_rsa:/var/jenkins_home/.ssh/id_rsa \
+            -v $HOME/.ssh/id_rsa.pub:/var/jenkins_home/.ssh/id_rsa.pub \
+            -v $HOME/.ssh/known_hosts:/var/jenkins_home/.ssh/known_hosts \
             -v $(pwd)/jenkins_home:/var/jenkins_home \
             -v /var/run/docker.sock:/var/run/docker.sock \
             -p 8080:8080 \
             -p 50000:50000 \
-            $IMAGE \
-            bash
+            $IMAGE 
+            
